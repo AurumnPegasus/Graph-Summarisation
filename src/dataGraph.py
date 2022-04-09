dd127a893f74c7d80e0783d80029ecea9c074d92
AurumnPegasus <subramanian.shivansh@gmail.com>
2022-04-09 11:56:55 +0530

    fixed graph, returning matrix

--- src/dataGraph.py
+++ src/dataGraph.py
@@ -3,7 +3,6 @@ from icecream import ic
 from constants import *
 import numpy as np
 from dataSplit import Data
-import networkx as nx
 from sentence_transformers import SentenceTransformer
 import math
 
@@ -11,20 +10,12 @@ class Graph:
     def __init__(self,
                  dataset):
         self.dataset = dataset
-        self.s2id = {}
-        self.id2s = {}
-        self.s_count = 0
 
         self.w2id = {}
         self.id2w = {}
         self.w2emb = {}
 
         self.encoder = SentenceTransformer('all-MiniLM-L6-v2')
-
-    def prepVocab(self):
-        df = pd.read_csv('./data/cache/CNNDM/vocab', delim_whitespace=True, names=["word", "idx"])
-        self.w2id = dict(df.values)
-        self.id2w = {v: k for k, v in self.w2id.items()}
 
     def prepEmbeddings(self):
         with open('./glove.6B.50d.txt', 'r') as f:
@@ -36,31 +27,37 @@ class Graph:
 
     def getGraph(self, sentences, edges):
 
-        G = nx.Graph()
-
         sentences = sentences['text']
         embeddings = self.encoder.encode(sentences)
-        sids = []
 
-        for sent, emb in zip(sentences, embeddings):
-            if sent not in self.s2id:
-                self.s2id[sent] = self.s_count
-                self.id2s[self.s_count] = sent
-                G.add_node(self.s_count, sent_feature=emb)
-                self.s_count += 1
+        vocab = set()
+        for edge in edges:
+            if isinstance(edge, dict):
+                for word in edge:
+                    vocab.add(word.lower())
+
+        word_embeddings = []
+        for word in vocab:
+            if word in self.w2emb:
+                word_embeddings.append(self.w2emb[word])
             else:
-                G.add_node(self.s2id[sent], sent_feature=emb)
+                word_embeddings.append(self.w2emb["unk"])
+        word_embeddings = np.array(word_embeddings)
 
-            sids.append(self.s2id[sent])
-            break
-
-        for i in range(len(edges)):
-            if edges[i] == np.nan:
-                ic("Here")
+        adjacency = []
+        for i in range(len(sentences)):
+            current = []
+            for word in vocab:
+                if word in edges[i]:
+                    current.append(edges[i][word])
+                else:
+                    current.append(0)
+            adjacency.append(current)
+        adjacency = np.array(adjacency)
+        ic(embeddings.shape, word_embeddings.shape, adjacency.shape)
 
     def handle(self):
         df = pd.read_json(path_or_buf='./data/cache/CNNDM/val.w2s.tfidf.jsonl', lines=True)
-        self.prepVocab()
         self.prepEmbeddings()
 
         for i in range(len(self.dataset)):
