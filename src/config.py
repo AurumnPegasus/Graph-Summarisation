cd363ffd6858bcd21029e3cf43482bca95982c7b
rahul <rahul008mehta@gmail.com>
2022-04-26 17:37:38 +0530

    config

--- src/config.py
+++ src/config.py
@@ -1,4 +1,9 @@
-SAMPLE_DATA_PATH = "./samples/sample.label.jsonl"
-SAMPLE_EDGE_PATH = "./samples/sample.w2s.tfidf.jsonl"
+SAMPLE_DATA_PATH = "../samples/sample.label.jsonl"
+SAMPLE_EDGE_PATH = "../samples/sample.w2s.tfidf.jsonl"
 
-EMBEDDING_PATH = "./data/glove.6B.50d.txt"
+EMBEDDING_PATH = "../data/glove.6B.50d.txt"
+#EMBEDDING_PATH = "../data/glove.6B.100d.txt"
+
+
+SAMPLE_TEST_DATA_PATH = "../samples/sample.label.jsonl"
+SAMPLE_TEST_EDGE_PATH = "../samples/sample.w2s.tfidf.jsonl"
